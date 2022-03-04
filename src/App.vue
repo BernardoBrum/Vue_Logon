6c4485311becb899c7017b12b4c3d730f8d88138
Bernardo Brum <bernardoalvesbrum_100@hotmail.com>
2022-03-03 21:24:22 -0300

    Feat: added sanitize

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
+  <div contenteditable="false" @paste="sanitize" id="app">
     <router-view />
   </div>
 </template>
 
 <script>
+export default {
+  methods: {
+    sanitize(event) {
+      event.preventDefault();
+      const html = this.$sanitize(event.clipboardData.getData('text/html'));
+      //or
+      //const html = this.$sanitize(
+      //  event.clipboardData.getData('text/html'),
+      //  {
+      //    allowedTags: ['b', 'br']
+      //  }
+      //);
+      document.execCommand('insertHTML', false, (html));
+    }
+  }
+}
 </script>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSanitize from "vue-sanitize";
 
+Vue.use(VueSanitize);
 Vue.config.productionTip = false
 
 new Vue({
